6f536c397f0a2a1c91e93d36f8c909c0e0403df4
kjxbyz <kjxbyz888@outlook.com>
2024-05-28 20:29:14 +0800

    initial commit

---
+++ Casks/picguard.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Cask-Cookbook
+#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+cask "picguard" do
+  version "1.0.0+240"
+  # https://docs.brew.sh/Cask-Cookbook#stanza-sha256
+  sha256 :no_check
+
+  # https://docs.brew.sh/Cask-Cookbook#stanza-url
+  url "https://github.com/picguard/picguard/releases/download/v#{version}/PicGuard.dmg"
+  name "PicGuard"
+  desc "Your pictures, your signature."
+  homepage "https://kjxbyz.com/picguard"
+
+  # Documentation: https://docs.brew.sh/Brew-Livecheck
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)(\+\d+)?/i)
+    strategy :github_latest
+  end
+
+  # https://docs.brew.sh/Cask-Cookbook#stanza-depends_on
+  depends_on macos: ">= :catalina"
+  depends_on arch: [:x86_64]
+
+  # https://docs.brew.sh/Cask-Cookbook#stanza-app
+  app "PicGuard.app"
+
+  # https://docs.brew.sh/Cask-Cookbook#stanza-uninstall
+  # uninstall quit: "com.kjxbyz.watermarkassistant.mac"
+
+  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
+  zap trash: []
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# homebrew-picguard
+
+## Install
+
+`brew install picguard/picguard/picguard`
+
+Or `brew tap picguard/picguard` and then `brew install picguard`.
+
+Or, in a [`brew bundle`](https://github.com/Homebrew/homebrew-bundle) `Brewfile`:
+
+```ruby
+tap "picguard/picguard"
+brew "picguard"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
